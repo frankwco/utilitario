0cd8eae9f33d51bc1a643702b21e19e412cfd5fe
frank <frankwco@gmail.com>
2021-07-08 19:47:23 -0300

    inicio

---
+++ src/main/java/com/example/utilitario/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.example.utilitario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
+
+	@PostMapping("/mostrar")
+	public String mostrarNome(@RequestParam(name = "nome") Integer nome, Model model) {
+		model.addAttribute("nn", nome);
+
+		System.out.println("Nome: " + nome);
+
+		return "index";
+	}
+}
